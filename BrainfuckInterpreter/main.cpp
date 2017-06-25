933f59a165a326fde7672916dc036c0b386237ef
Frank <fabu@fblz.de>
2017-06-25 17:16:01 +0200

    Revert "Merge pull request #1 from Frzifus/master"
    This reverts commit 56e6303bde57a3c8023406578272b6c0c1b8532e, reversing
    changes made to d6f931e0025c8bc39a62261d95a50934bc326a7a.

--- BrainfuckInterpreter/main.cpp
+++ BrainfuckInterpreter/main.cpp
@@ -1,48 +1,43 @@
-#include "./brainfuck.h"
+
 
 #include <iostream>
 #include <string>
+#include "Brainfuck.h"
 
-int main(int argc, char *argv[]) {
-  std::string code =
-      ""
-      "  +++++ +++++             initialize counter (cell #0) to 10"
-      "  [                       use loop to set 70/100/30/10"
-      "      > +++++ ++              add  7 to cell #1"
-      "      > +++++ +++++           add 10 to cell #2"
-      "      > +++                   add  3 to cell #3"
-      "      > +                     add  1 to cell #4"
-      "  <<<< -                  decrement counter (cell #0)"
-      "  ]"
-      "  > ++ .                  print 'H'"
-      "  > + .                   print 'e'"
-      "  +++++ ++ .              print 'l'"
-      "  .                       print 'l'"
-      "  +++ .                   print 'o'"
-      "  > ++ .                  print ' '"
-      "  << +++++ +++++ +++++ .  print 'W'"
-      "  > .                     print 'o'"
-      "  +++ .                   print 'r'"
-      "  ----- - .               print 'l'"
-      "  ----- --- .             print 'd'"
-      "  > + .                   print '!'"
-      "  > .                     print '\n'";
+int main(int argc, char* argv[])
+{
+	std::string code = ""
+		"  +++++ +++++             initialize counter (cell #0) to 10"
+		"  [                       use loop to set 70/100/30/10"
+		"      > +++++ ++              add  7 to cell #1"
+		"      > +++++ +++++           add 10 to cell #2"
+		"      > +++                   add  3 to cell #3"
+		"      > +                     add  1 to cell #4"
+		"  <<<< -                  decrement counter (cell #0)"
+		"  ]"
+		"  > ++ .                  print 'H'"
+		"  > + .                   print 'e'"
+		"  +++++ ++ .              print 'l'"
+		"  .                       print 'l'"
+		"  +++ .                   print 'o'"
+		"  > ++ .                  print ' '"
+		"  << +++++ +++++ +++++ .  print 'W'"
+		"  > .                     print 'o'"
+		"  +++ .                   print 'r'"
+		"  ----- - .               print 'l'"
+		"  ----- --- .             print 'd'"
+		"  > + .                   print '!'"
+		"  > .                     print '\n'";
 
-  Brainfuck Interpreter(code);
-  if (Interpreter.run()) {
-    std::cout << "Success!" << std::endl;
+	Brainfuck Interpreter(code);
+	if(Interpreter.run())
+	{
+		std::cout << "Success!" << std::endl;
+		system("pause");
+		return 0;
+	}
 
-#ifdef _WIN32
-    system("pause");
-#endif // _WIN32
-
-    return 0;
-  }
-
-  std::cout << "Failure!" << std::endl;
-#ifdef _WIN32
-  system("pause");
-#endif // _WIN32
-
-  return 1;
+	std::cout << "Failure!" << std::endl;
+	system("pause");
+	return 1;
 }
